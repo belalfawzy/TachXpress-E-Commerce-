a4d5f1480825a1173bd9f974e25d1b0875733d3f
YASMEEN <147400556+YASMEENELSHAMY@users.noreply.github.com>
2025-04-25 03:16:26 +0300

    Add files via upload

---
+++ TechXpress-DepiGraduation/Controllers/CategoriesController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TechXpress_DepiGraduation.Data.Services;
+using TechXpress_DepiGraduation.Models;
+
+namespace TechXpress_DepiGraduation.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryservice;
+        public CategoriesController(ICategoryService service)
+        {
+            _categoryservice = service;
+                
+        }
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryservice.GetAllAsync();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Name,Description")]Category category)
+        {
+            ModelState.Remove("Products");
+
+            if (ModelState.IsValid)
+            {
+                await _categoryservice.CreateAsync(category);
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(category);
+
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            var category = await _categoryservice.GetItemByIdAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            var category = await _categoryservice.GetItemByIdAsync(id);
+            if (category == null) return NotFound();
+            return View(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit([Bind("Id,Name,Description")] Category category)
+        {
+           
+            ModelState.Remove("Products");
+
+            if (ModelState.IsValid)
+            {
+                await _categoryservice.EditAsync(category);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _categoryservice.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+    }
+}
